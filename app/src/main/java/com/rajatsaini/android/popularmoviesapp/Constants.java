da80094820a09d79256e9b17141d96aca0423df8
rajatsaini0294 <rajatkumar0294@gmail.com>
2016-04-17 22:03:54 +0530

    Commit

--- app/src/main/java/com/rajatsaini/android/popularmoviesapp/Constants.java
+++ app/src/main/java/com/rajatsaini/android/popularmoviesapp/Constants.java
@@ -4,5 +4,5 @@ package com.rajatsaini.android.popularmoviesapp;
  * Created by rajat on 3/30/2016.
  */
 public class Constants {
-    public final static String API_KEY = " your api key ";
+    public final static String API_KEY = " your api key here";
 }
